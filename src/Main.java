580955abe43c5a8ba65af7489e036d03315afbfa
sidor <6hr@de6U>
2025-01-14 11:56:21 +0300

    class car

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,13 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+
+
+
         }
+        Car car = new Car();
+        car.beepBeep();
+        car.setModel("Granta");
+        System.out.println(car.getModel());
     }
 }
