f59c6a0c72d9d1f858caf74a455cec3e90cc75f5
sidor <6hr@de6U>
2025-01-14 18:59:25 +0300

    lesson12

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,64 @@
+import java.util.Arrays;
+import java.util.Collections;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+// Задание: Создание массива игроков напрямую + посчитать число элементов + вывести + создать массив должностей
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String[] players = new String[10];
+        players[0] = "Лунёв";
+        players[1] = "Лещук";
+        players[2] = "Скопинцев";
+        players[3] = "ЭлиДаса";
+        players[4] = "Диего";
+        players[5] = "Гомес";
+        players[6] = "Нгамалё";
+        players[7] = "Смолов";
+        players[8] = "Бальбуэна";
+        players[9] = "Гагнидзе";
+        System.out.println("Сегодня будем смотреть игру Динамо - Спартак!");
+        System.out.println("Команда Динамо на игру: " + Arrays.asList(players));
+        System.out.println("На дружеский матч со спартаком Динамо привезли всего лишь " + players.length + " игроков");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String[] roles = new String[]{"Вратарь", "Защитник", "Полузащитник", "Нападающий"};
+        System.out.println("В команде Динамо есть: " + Arrays.asList(roles) + "\n");
 
 
+// Задание: создане массива тренеров используя конструктов из предыдущих уроков с несколькими параметрами
+//    Высчитать минимальное и максимальное время работы за команду
+
+
+        Team[] coachs = new Team[3];
+        coachs[0] = new Team("Russia", 365);
+        coachs[1] = new Team("USA", 520);
+        coachs[2] = new Team("Brazilia", 180);
+
+        System.out.println("Количество тренеров = " + coachs.length);
+        System.out.println(Arrays.asList(coachs));  //                                                              <-------------- выводит не значение, а хэши
+
+//TODO - не понимаю почему не работет вывод массива - строчка выше
+
+
+        for (int i = 0; i < coachs.length; i++) {
+            Team countruCoach = coachs[i];
+            if (countruCoach.getCountry().equals("Brazilia")) {
+                System.out.println("Среди них найден бразилец");
+                break;
+            }
 
         }
-        Car car = new Car();
-        car.beepBeep();
-        car.setModel("Granta");
-        System.out.println(car.getModel());
+        for (int i = coachs.length - 1; i >= 0; i--) {
+            Team countruCoach = coachs[i];
+            if (countruCoach.getCountry().equals("Russia")) {
+                System.out.println("Все таки хоть один русский тренер есть. Это - " + countruCoach.toString()); // <------------- тут я наверно не совсем понял про toString
+                break;
+            }
+
+        }
     }
+
+
+
 }
