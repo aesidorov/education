cf03539e8a13b9aea7063e086a7ddc802567e93f
sidor <6hr@de6U>
2025-01-14 11:25:16 +0300

    class car

---
+++ src/Car.java
@@ -0,0 +1,26 @@
+public class Car {
+
+    private String model;
+
+    private int Fuel;
+
+    public void beepBeep(){
+        System.out.println("Машина посигналила");
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getFuel() {
+        return Fuel;
+    }
+
+    public void setFuel(int fuel) {
+        Fuel = fuel;
+    }
+}
